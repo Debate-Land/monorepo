a6d60080d1312243a4ebbac557d04865045a1948
http-samc <67826352+http-samc@users.noreply.github.com>
2023-04-01 18:57:11 -0500

    Expanding subcomponents

--- apps/web/src/components/Table.tsx
+++ apps/web/src/components/Table.tsx
@@ -1,7 +1,8 @@
+import { Text } from '@shared/components';
 import { Alias } from '@shared/database';
 import { trpc } from '@src/utils/trpc'
-import { createColumnHelper, flexRender, getCoreRowModel, PaginationState, useReactTable } from '@tanstack/react-table';
-import React, { useMemo, useState } from 'react'
+import { createColumnHelper, ExpandedState, flexRender, getCoreRowModel, getExpandedRowModel, getPaginationRowModel, PaginationState, Row, useReactTable } from '@tanstack/react-table';
+import React, { Fragment, useMemo, useState } from 'react'
 
 type Data = {
   team: {
@@ -11,11 +12,31 @@ type Data = {
   otr: number;
 };
 
-const Table = () => {
+interface TableProps {
+  renderSubComponent?: ({row}: {row: Row<Data>}) => JSX.Element;
+}
+
+interface SubComponentProps {
+  row: Data
+}
+
+const SubComponent = ({row}: SubComponentProps) => {
+  const { data } = trpc.team.useQuery({
+    event: 'PublicForum',
+    id: row.team.id,
+  });
+
+  return data
+    ? <>{JSON.stringify(data)}</>
+    : <>Loading...</>
+};
+
+const Table = ({renderSubComponent: _}: TableProps) => {
   const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: 10,
   });
+  // const [expanded, setExpanded] = useState<ExpandedState>({});
 
   const { data } = trpc.leaderboard.useQuery(
     {
@@ -37,6 +58,15 @@ const Table = () => {
   const column = createColumnHelper<Data>();
 
   const columns = [
+    column.display({
+      id: "expander",
+      header: "Details",
+      cell: ({ row }) => (
+        row.getCanExpand()
+          ? <button onClick={row.getToggleExpandedHandler()}>{row.getIsExpanded() ? '-' : '+'}</button>
+          : <>--</>
+      )
+    }),
     column.accessor("otr", {
       header: "OTR",
       cell: props => props.getValue().toFixed(3)
@@ -51,13 +81,18 @@ const Table = () => {
     data: data || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    getRowCanExpand: () => true,
     pageCount: -1,
     state: {
-      pagination
+      pagination,
+      // expanded
     },
     onPaginationChange: setPagination,
+    // onExpandedChange: setExpanded,
     manualPagination: true,
-  })
+  });
 
   return (
     <>
@@ -92,17 +127,30 @@ const Table = () => {
         {
           table.getRowModel().rows.map(
             row => (
-              <tr key={row.id}>
-                {
-                  row.getVisibleCells().map(
-                    cell => (
-                      <td key={cell.id}>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </td>
+              <Fragment key={row.id}>
+                {/* Actual table row */}
+                <tr>
+                  {
+                    row.getVisibleCells().map(
+                      cell => (
+                        <td key={cell.id}>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </td>
+                      )
                     )
+                  }
+                </tr>
+                {/* Expanded data housed in additional row, if available */}
+                {
+                  row.getIsExpanded() && (
+                    <tr>
+                      <td colSpan={row.getVisibleCells().length}>
+                        <SubComponent row={row.original} />
+                      </td>
+                    </tr>
                   )
                 }
-              </tr>
+              </Fragment>
             )
           )
         }
@@ -136,6 +184,7 @@ const Table = () => {
       </table>
       <button onClick={table.previousPage}>{'<'}</button>
       <button onClick={table.nextPage}>{'>'}</button>
+      <Text>Page {table.getState().pagination.pageIndex + 1} of --</Text>
 
     </>
   )
