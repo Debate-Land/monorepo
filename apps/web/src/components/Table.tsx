1020c1da65c59573c5af81157ddf8566af2b96e1
http-samc <67826352+http-samc@users.noreply.github.com>
2023-04-02 00:53:13 -0500

    Condensed API

--- apps/web/src/components/Table.tsx
+++ apps/web/src/components/Table.tsx
@@ -11,99 +11,107 @@ interface TableProps<T> {
 
 // FIXME: Row idx staying open after pagination...
 // Opt-in pagination, expandable if appropriate column is supplied.
-const Table = <T,>({ definition: table, child: ExpandedRow }: TableProps<T>) => (
-  <div>
-    <table className="table-auto">
-      <thead>
-        {
-          table.getHeaderGroups().map(
-            headerGroup => (
-              <tr key={headerGroup.id}>
-                {
-                  headerGroup.headers.map(
-                    header => (
-                      <th key={header.id}>
-                        {
-                          header.isPlaceholder
-                            ? null
-                            : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )
-                        }
-                      </th>
-                    )
-                  )
-                }
-              </tr>
-            )
-          )
-        }
-      </thead>
-      <tbody>
-        {
-          table.getRowModel().rows.map(
-            row => (
-              <Fragment key={row.id}>
-                {/* Actual table row */}
-                <tr>
+const Table = <T,>({ definition: table, child: ExpandedRow }: TableProps<T>) => {
+  const currentPage = table.getState().pagination?.pageIndex;
+
+  useEffect(() => {
+    table.resetExpanded(false);
+  }, [table, currentPage]);
+
+  return (
+    <div>
+      <table className="table-auto">
+        <thead>
+          {
+            table.getHeaderGroups().map(
+              headerGroup => (
+                <tr key={headerGroup.id}>
                   {
-                    row.getVisibleCells().map(
-                      cell => (
-                        <td key={cell.id}>
-                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                        </td>
+                    headerGroup.headers.map(
+                      header => (
+                        <th key={header.id}>
+                          {
+                            header.isPlaceholder
+                              ? null
+                              : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )
+                          }
+                        </th>
                       )
                     )
                   }
                 </tr>
-                {/* Expanded data housed in additional row, if available */}
-                {
-                  row.getIsExpanded() && ExpandedRow && (
-                    <tr>
-                      <td colSpan={row.getVisibleCells().length}>
-                        <ExpandedRow row={row.original} />
-                      </td>
-                    </tr>
-                  )
-                }
-              </Fragment>
+              )
             )
-          )
-        }
-      </tbody>
-      <tfoot>
-        {
-          table.getFooterGroups().map(
-            footerGroup => (
-              <tr key={footerGroup.id}>
-                {
-                  footerGroup.headers.map(
-                    header => (
-                      <th key={header.id}>
-                        {
-                          header.isPlaceholder
-                            ? null
-                            : flexRender(
-                              header.column.columnDef.footer,
-                              header.getContext()
-                            )
-                        }
-                      </th>
+          }
+        </thead>
+        <tbody>
+          {
+            table.getRowModel().rows.map(
+              row => (
+                <Fragment key={row.id}>
+                  {/* Actual table row */}
+                  <tr>
+                    {
+                      row.getVisibleCells().map(
+                        cell => (
+                          <td key={cell.id}>
+                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                          </td>
+                        )
+                      )
+                    }
+                  </tr>
+                  {/* Expanded data housed in additional row, if available */}
+                  {
+                    row.getIsExpanded() && ExpandedRow && (
+                      <tr>
+                        <td colSpan={row.getVisibleCells().length}>
+                          <ExpandedRow row={row.original} />
+                        </td>
+                      </tr>
                     )
-                  )
-                }
-              </tr>
+                  }
+                </Fragment>
+              )
             )
-          )
-        }
-      </tfoot>
-    </table>
-    <button onClick={table.previousPage}>{'<'}</button>
-    <button onClick={table.nextPage}>{'>'}</button>
-    <Text>Page {table.getState().pagination.pageIndex + 1} of --</Text>
-  </div>
-);
+          }
+        </tbody>
+        <tfoot>
+          {
+            table.getFooterGroups().map(
+              footerGroup => (
+                <tr key={footerGroup.id}>
+                  {
+                    footerGroup.headers.map(
+                      header => (
+                        <th key={header.id}>
+                          {
+                            header.isPlaceholder
+                              ? null
+                              : flexRender(
+                                header.column.columnDef.footer,
+                                header.getContext()
+                              )
+                          }
+                        </th>
+                      )
+                    )
+                  }
+                </tr>
+              )
+            )
+          }
+        </tfoot>
+      </table>
+      <button onClick={table.previousPage}>{'<'}</button>
+      <button onClick={table.nextPage}>{'>'}</button>
+      <Text>Page {table.getState().pagination.pageIndex + 1} of --</Text>
+    </div>
+  );
+};
 
 interface UseCustomTableOptions<T> {
   data: T[] | undefined;
@@ -186,32 +194,27 @@ const LeaderboardTable = () => {
   );
 
   const column = createColumnHelper<Data>();
-  const table = useCustomTable({
-    data,
-    columns: [
-      getExpandingColumn<Data>(),
-      column.accessor("otr", {
-        header: "OTR",
-        cell: props => props.getValue().toFixed(3)
-      }),
-      column.accessor("team.aliases", {
-        header: "Team",
-        cell: props => props.getValue()[0].code
-      })
-    ] as ColumnDef<Data>[],
-    paginationConfig: {
-      paginationState: pagination,
-      setPaginationState: setPagination,
-    }
-  });
-
-  useEffect(() => {
-    table.resetExpanded(false)
-  }, [table, pagination.pageIndex]);
 
   return <Table<Data>
     definition={
-      table
+      useCustomTable({
+        data,
+        columns: [
+          getExpandingColumn<Data>(),
+          column.accessor("otr", {
+            header: "OTR",
+            cell: props => props.getValue().toFixed(3)
+          }),
+          column.accessor("team.aliases", {
+            header: "Team",
+            cell: props => props.getValue()[0].code
+          })
+        ] as ColumnDef<Data>[],
+        paginationConfig: {
+          paginationState: pagination,
+          setPaginationState: setPagination,
+        }
+      })
     }
     child={SubComponent}
   />
