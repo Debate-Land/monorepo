9c98f23860c50a2dd47bdc407f67d56356b62e0e
http-samc <67826352+http-samc@users.noreply.github.com>
2023-04-03 10:03:44 -0500

    Revert "Update [id].tsx"
    This reverts commit 9b7e8da60a8c7d32f2aae0abe8efa9446080a8ed.

--- apps/web/src/pages/[event]/teams/[id].tsx
+++ apps/web/src/pages/[event]/teams/[id].tsx
@@ -23,107 +23,98 @@ const Team = () => {
     }
   );
 
-  console.log(data?.statistics)
+  if (!data) return;
 
   return (
     <>
       <NextSeo
-        title={`Debate Land: ${data?.aliases[0]?.code || '--'}'s Profile`}
-        description={`${data?.aliases[0].code || '--'}'s competitive statistics in ${query.event}, exclusively on Debate Land.`}
+        title={`Debate Land: ${data.aliases[0]?.code}'s Profile`}
+        description={`${data.aliases[0].code}'s competitive statistics in ${query.event}, exclusively on Debate Land.`}
         additionalLinkTags={[
           {
             rel: 'icon',
             href: '/favicon.ico',
           },
         ]}
-        noindex
       />
       <div className="min-h-screen">
         <Overview
           label="Team"
           heading={
-            data
-              ? (
-                <>
-                  <a href={`/competitors/${data.competitors[0].id}`}>
-                    {data.competitors[0].name}
-                  </a>
-                  {
-                    data.competitors.length > 1 && (
-                      <span>
-                        {' & '}
-                        <a href={`/competitors/${data.competitors[1].id}`}>
-                          {data.competitors[1].name}
-                        </a>
-                      </span>
-                    )
-                  }
-                </>
-              )
-              : 'LOADER'
+            <>
+              <a href={`/competitors/${data.competitors[0].id}`}>
+                {data.competitors[0].name}
+              </a>
+              {
+                data.competitors.length > 1 && (
+                  <span>
+                    {' & '}
+                    <a href={`/competitors/${data.competitors[1].id}`}>
+                      {data.competitors[1].name}
+                    </a>
+                  </span>
+                )
+              }
+            </>
           }
-          subtitle={
-            data
-              ? `${query.event} | ${data.circuits[0].name} | ${data.seasons[0].id}-${data.seasons[data.seasons.length - 1].id}`
-              : 'LOADER'
-            }
+          subtitle={`${query.event} | ${data.circuits[0].name} | ${data.seasons[0].id}-${data.seasons[data.seasons.length - 1].id}`}
           underview={
             <Statistics
               primary={[
                 {
-                  value: data ? Math.round(data.statistics.otr * 100) / 100 : undefined,
+                  value: Math.round(data.statistics.otr * 100) / 100,
                   description: "OTR Score"
                 },
                 {
-                  value: data ? data.statistics.inTop20Pct + 'x' : undefined,
+                  value: data.statistics.inTop20Pct + 'x',
                   description: "Top 20% Prelim Seed"
                 },
                 {
-                  value: data?.statistics.bids,
-                  description: `TOC Bid${(data?.statistics.bids || 2) > 1 ? 's' : ''}`
+                  value: data.statistics.bids,
+                  description: `TOC Bid${data.statistics.bids > 1 ? 's' : ''}`
                 },
                 {
-                  value: data ? Math.round(data.statistics.avgSpeaks * 10) / 10 : undefined,
+                  value: Math.round(data.statistics.avgSpeaks * 10) / 10,
                   description: "Avg Raw Spks."
                 }
               ]}
               advanced={[
                 {
-                  value: data?.results.length,
+                  value: data.results.length,
                   description: "Tournaments"
                 },
                 {
-                  value: data?._count.rounds,
+                  value: data._count.rounds,
                   description: "Rounds"
                 },
                 {
-                  value: data?.statistics.lastActive,
+                  value: data.statistics.lastActive,
                   description: "Last Active"
                 },
                 {
-                  value: data?.statistics.avgOpWpM,
-                  isPercentage: false,
+                  value: data.statistics.avgOpWpM,
+                  isPercentage: true,
                   round: 1,
                   description: "Avg. OpWpM"
                 },
                 {
-                  value: data ? `${data.statistics.pRecord[0]}-${data.statistics.pRecord[1]}` : undefined,
+                  value: `${data.statistics.pRecord[0]}-${data.statistics.pRecord[1]}`,
                   description: "Prelim Rcd."
                 },
                 {
-                  value: data?.statistics.pWp,
+                  value: data.statistics.pWp,
                   isPercentage: true,
                   round: 1,
                   description: "Prelim Win Pct."
                 },
                 {
-                  value: data?.statistics.breakPct,
+                  value: data.statistics.breakPct,
                   isPercentage: true,
                   round: 1,
                   description: "Break Pct."
                 },
                 {
-                  value: data?.statistics.tWp,
+                  value: data.statistics.tWp,
                   isPercentage: true,
                   round: 1,
                   description: "True Win Pct."
@@ -132,7 +123,7 @@ const Team = () => {
             />
           }
         />
-        <TournamentListTable data={data?.results} />
+        <TournamentListTable data={data.results} />
         {/* TODO: Alias/School Tables & Charts */}
       </div>
     </>
