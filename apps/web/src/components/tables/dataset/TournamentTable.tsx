91f8465384b6bf4b65368b75bdb14c8c5b942261
http-samc <67826352+http-samc@users.noreply.github.com>
2023-04-13 15:47:35 -0500

    fixed tournament table

--- apps/web/src/components/tables/dataset/TournamentTable.tsx
+++ apps/web/src/components/tables/dataset/TournamentTable.tsx
@@ -8,7 +8,7 @@ import { ColumnDef, createColumnHelper, PaginationState } from '@tanstack/react-
 
 type TournamentTableRow = Tournament & {
   _count: {
-      results: number;
+      teamResults: number;
   };
 }
 
@@ -53,7 +53,7 @@ const TournamentTable = ({count}: TournamentTableProps) => {
             }),
           ] as ColumnDef<TournamentTableRow>[],
           sm: [
-            column.accessor('_count.results', {
+            column.accessor('_count.teamResults', {
               header: "Entries",
               cell: props => props.getValue()
             })
