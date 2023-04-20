18ab007c475f35c784ea4b987b3ac7ba879ad636
http-samc <67826352+http-samc@users.noreply.github.com>
2023-04-20 13:43:41 -0500

    add CMS

---
+++ packages/cms/src/schemas/index.ts
@@ -0,0 +1,12 @@
+import author from "./author";
+import blockContent from "./block-content";
+import post from "./post";
+import siteConfig from "./site-config";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  author,
+  blockContent,
+  post,
+  siteConfig
+]
