01ccb65a3630d5602dce22cba40fa9e4d771783c
http-samc <67826352+http-samc@users.noreply.github.com>
2023-04-24 02:33:37 -0500

    scaffolding

---
+++ apps/web/src/server/routers/issueRouter.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+import { procedure, router } from '../trpc';
+import { LinearClient } from '@linear/sdk';
+
+const issueRouter = router({
+  create: procedure
+    .input(z.object({
+      email: z.string(),
+      name: z.string(),
+      description: z.string(),
+    }))
+    .mutation(async ({ input }) => {
+      const client = new LinearClient({
+        // eslint-disable-next-line turbo/no-undeclared-env-vars
+        apiKey: process.env.NEXT_PUBLIC_LINEAR_KEY
+      });
+
+      const teams = await client.teams();
+      const team = teams.nodes[0];
+      // const labels = await team.labels();
+
+      await client.createIssue({
+        teamId: team.id,
+        title: input.name,
+        description: `${input.description} (Submitted by ${input.email})`,
+        // labelIds: [
+
+        // ]
+      })
+    })
+});
+
+export default issueRouter;
