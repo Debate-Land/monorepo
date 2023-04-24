92a33c8ad3fa06ea8686800db58a3c899e717d6b
http-samc <67826352+http-samc@users.noreply.github.com>
2023-04-24 12:16:48 -0500

    Update revalidate.ts

--- apps/web/src/pages/api/revalidate.ts
+++ apps/web/src/pages/api/revalidate.ts
@@ -3,14 +3,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { types } from '@shared/cms';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const signature = req.headers[SIGNATURE_HEADER_NAME]! as string;
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  const isValid = isValidSignature(JSON.stringify(req.body), signature, process.env.SANITY_WEBHOOK_SIGNATURE!);
+  // TODO: Use Sanity signature validation.
 
-  // Validate signature
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
+  const isValid = (req.headers['sanity-webhook-signature']! as string).split(',')[0] == process.env.SANITY_WEBHOOK_SIGNATURE!;
+
   if (!isValid) {
-    // eslint-disable-next-line turbo/no-undeclared-env-vars
-    res.send(`${signature} ${process.env.SANITY_WEBHOOK_SIGNATURE!}`);
+    res.status(404)
     return;
   }
 
@@ -18,13 +17,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const document = req.body as unknown as types.Page;
     const pathToRevalidate = document.slug.current;
 
-    await res.revalidate(`${document.pageType === 'blog-post' ? 'blog' : ''}/${pathToRevalidate}`);
-
+    await res.revalidate(`${document.pageType === 'blog-post' ? '/blog' : ''}/${pathToRevalidate}`);
     return res.json({ revalidated: true });
   }
   catch (err) {
-    // Could not revalidate. The stale page will continue to be shown until
-    // this issue is fixed.
     return res.status(500).send('Error while revalidating');
   }
 }
