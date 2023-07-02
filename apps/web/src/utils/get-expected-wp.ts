27bc621fd5e4a6479c0815c37e8fc5819adca78e
http-samc <67826352+http-samc@users.noreply.github.com>
2023-07-02 08:22:21 -0500

    predictive links for all rounds

--- apps/web/src/utils/get-expected-wp.ts
+++ apps/web/src/utils/get-expected-wp.ts
@@ -4,5 +4,5 @@ export default function getExpectedWP(otr1: number, otr2: number, avgJudgeIndex?
 
   let index = avgJudgeIndex !== undefined && !Number.isNaN(avgJudgeIndex) ? avgJudgeIndex : 10;
 
-  return ((1.47 * Math.pow(deltaOtr, 0.8094)) * (1 / (4 * avgOtr))) / (1 + Math.pow(2, -(index - 3))) + 0.5;
+  return ((1.47 * Math.pow(deltaOtr, 0.8094)) * (1 / (4 * avgOtr))) / (1 + Math.pow(2, -(index - 6))) + 0.5;
 }
